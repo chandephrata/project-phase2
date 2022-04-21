a9a04b3a8baa4ced452324fcf507b1c039a5b7e4
Joe <zvmboni@gmail.com>
2022-04-21 12:00:20 -0700

    fixed fooddetail

--- src/components/FoodDetail.js
+++ src/components/FoodDetail.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function FoodDetail({currentDishDetail}) {
+function FoodDetail({currentDish, currentDishDetail}) {
   return (
     <div>FoodDetail: {currentDishDetail}
 
