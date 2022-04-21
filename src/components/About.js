9d3a0d4225824598f11009ec41436accef0ba81f
Joe <zvmboni@gmail.com>
2022-04-21 15:38:24 -0700

    added about message

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
 function About() {
-  // text/pic page about us, no functionality
   return (
-    <div>About</div>
+    <div className='container'>
+      <h1>About Us</h1>
+      <h2>Stephen Simmons</h2>
+      <p>asdfasdf</p>
+      <p>---</p>
+      <h2>Chandelier Ephrata</h2>
+      <p>asdfasdf</p>
+      <p>---</p>
+      <h2>Joe Lee</h2>
+      <p>I've worked on a food truck for a few years up and down the west coast, but I'm ready to move to fine dining so y'all can catch me at Sim's Cafe!</p>
+    </div>
   )
 }
 
