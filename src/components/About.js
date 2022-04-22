274af4abea3544eef817960446bd4ceb3277fcc0
Chandelier Ephrata <chandephrata@gmail.com>
2022-04-22 11:41:08 -0600

    About

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ function About() {
       <p> blurb </p>
       <h2>Chandelier Ephrata</h2>
       <p>Sous-Chef 👩🏿‍🍳 </p>
-      <p>I have been a sous-chef at Sim's cafe for 4 years, and we are the business of making people happy through food.</p>
+      <p>I have been a sous-chef at Sim's cafe for 4 years, and we are in the business of making people happy through food.</p>
       <h2>Joe Lee</h2>
       <p>I've worked on a food truck for a few years up and down the west coast, but I'm ready to move to fine dining so y'all can catch me at Sim's Cafe!</p>
     </div>
