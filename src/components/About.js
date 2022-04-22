6c4346ffbf97b6d054342d47ff3a3f4ef7189086
Stephen Simmons <stephensimmons@stephens-MacBook-Pro.local>
2022-04-22 11:35:58 -0700

    about update

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ function About() {
       <h1>About Us</h1>
       <h2>Stephen Simmons</h2>
       <p>Head Chef</p>
-      <p> blurb </p>
+      <p> I attended La Cordon Bleu San Francisco at the age of 19 after graduation  I moved to  Louisiana for a few year so I can learn to  cook all things  Cajun and  Creole this food was so rich and diverse this is the original fusion food in the united states and has so much depth that I can spend years learning and masting the food as a head chef I will take you on a adventure packed journey with every bite to come spend some time at Sim’s Cafe </p>
       <h2>Chandelier Ephrata</h2>
       <p>Sous-Chef 👩🏿‍🍳 </p>
       <p>I have been a sous-chef at Sim's cafe for 4 years, and we are the business of making people happy through food.</p>
